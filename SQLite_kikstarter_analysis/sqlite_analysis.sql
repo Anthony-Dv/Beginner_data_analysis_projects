3fd955166b047613a57fe08b3f2620de1d16ab5d
Anthony-Dv <dvoracekantonin@icloud.com>
2023-09-27 10:17:26 +0200

    updating sqlite_analysis.sql

--- SQLite_kikstarter_analysis/sqlite_analysis.sql
+++ SQLite_kikstarter_analysis/sqlite_analysis.sql
@@ -22,11 +22,11 @@ SELECT main_category, COUNT(main_category)
 -- Distribution of main_category's 26 at least 95% funded projects is:
 Technology	    7
 Design	        7
-Music	        4
-Film & Video	4
-Games	        3
+Music	          4
+Film & Video	  4
+Games	          3
 Theater	        2
-Food	        2
+Food	          2
 Fashion	        2
 Publishing	    1
 Crafts	        1
@@ -49,8 +49,8 @@ SELECT main_category, COUNT(main_category)
 -- Distribution of main_category's 79 successful projects which got more the 50 times their goal is:
 Design	    37
 Technology	20
-Games	    18
-Food	    2
+Games	      18
+Food	      2
 Fashion	    2
 
 
@@ -71,19 +71,19 @@ SELECT main_category, COUNT(main_category)
 
 -- Distribution of main_category's 19624 worst projects is:
 Film & Video	3538
-Technology	    3063
-Design	        2636
+Technology	  3063
+Design	      2636
 Games	        2468
 Food	        1775
-Publishing	    1491
-Fashion	        1279
+Publishing	  1491
+Fashion	      1279
 Music	        1146
-Art	            788
-Photography	    349
-Theater	        287
-Comics	        286
-Crafts	        240
-Journalism	    209
+Art	          788
+Photography	  349
+Theater	      287
+Comics	      286
+Crafts	      240
+Journalism	  209
 Dance	        69
 */
 -- Last but not least lets look at what type of projects are most and least common on kikstarter
@@ -95,16 +95,16 @@ SELECT main_category, COUNT(main_category)
 -- Distribution of main_category on kikstarter is:
 Film & Video	63585
 Music	        51918
-Publishing	    39874
+Publishing	  39874
 Games	        35231
-Technology	    32569
-Design	        30070
-Art	            28153
+Technology	  32569
+Design	      30070
+Art	          28153
 Food	        24602
-Fashion	        22816
-Theater	        10913
-Comics	        10819
-Photography	    10779
-Crafts	        8809
-Journalism	    4755
+Fashion	      22816
+Theater	      10913
+Comics	      10819
+Photography	  10779
+Crafts	      8809
+Journalism	  4755
 Dance	        3768
